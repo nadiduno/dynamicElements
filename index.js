ce6e99274156b699099b2f31b407b874d84311b1
nadiduno <nadiduno@gmail.com>
2022-12-14 17:03:50 -0300

    ShowJavaScript

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
-// Import stylesheets
-import './style.css';
-// document.body.onload = adcElemento;
-
 // Write Javascript code!
 const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>Conteúdo de uma Classe de Livros e Categorias</h1>`;
+appDiv.innerHTML = `<h2>Categorias de Livros</h2>`;
 const booksByCategory = [
+  {
+    category: 'Infantil',
+    books: [
+      {
+        title: 'O pequeno principe',
+        author: 'Antoine de Saint-Exupéry',
+        coverBook: '',
+      },
+      {
+        title: 'Bisa Bia, Bisa Bel',
+        author: 'Ana Maria Machado',
+      },
+    ],
+  },
   {
     category: 'Riqueza',
     books: [
       {
-        title: 'Os segredos da mente milionária',
-        author: 'T. Harv Eker',
+        title: 'Mulher Rica',
+        author: 'Kim Kiyosaki',
       },
       {
         title: 'O homem mais rico da Babilônia',
@@ -22,8 +32,8 @@ const booksByCategory = [
         author: 'Robert T. Kiyosaki e Sharon L. Lechter',
       },
       {
-        title: 'Mulher Rica',
-        author: 'Kim Kiyosaki',
+        title: 'Os segredos da mente milionária',
+        author: 'T. Harv Eker',
       },
     ],
   },
@@ -45,22 +55,41 @@ const booksByCategory = [
     ],
   },
 ];
+
+function livrosCategory(recorrido) {
+  let i = 1;
+
+  for (let recorrido2 of recorrido.books) {
+    var spanNova = document.createElement('span');
+    var livroNovo = document.createTextNode(
+      '\nLivro ' + i + ': ' + recorrido2.title + ' por ' + recorrido2.author
+    );
+    spanNova.appendChild(livroNovo);
+    var spanAtual = document.getElementById('complemento');
+    document.body.insertBefore(spanNova, spanAtual);
+    i++;
+  }
+}
 const cate = document.getElementById('category');
 var elemento;
 for (let recorrido of booksByCategory) {
-  // elemento=recorrido.category
-  var divNova = document.createElement("div");
-  var conteudoNovo = document.createTextNode('Categoria: ' + recorrido.category);
+  var divNova = document.createElement('div');
+  var conteudoNovo = document.createTextNode(recorrido.category);
   divNova.appendChild(conteudoNovo);
-  var divAtual = document.getElementById("category");
+  var divAtual = document.getElementById('category');
   document.body.insertBefore(divNova, divAtual);
 
-  var pNova = document.createElement("p");
-  var numNovo = document.createTextNode('Número de livros: ' + recorrido.books.length);
+  var pNova = document.createElement('p');
+  var numNovo = document.createTextNode(
+    'Número de livros registrados para esta categoria: ' +
+      recorrido.books.length
+  );
   pNova.appendChild(numNovo);
-  var pAtual = document.getElementById("complemento");
+  var pAtual = document.getElementById('complemento');
   document.body.insertBefore(pNova, pAtual);
 
-  // cate.innerHTML = `<li>${recorrido.category}</li>`;
+  livrosCategory(recorrido);
 }
 
+// Import stylesheets
+import './style.css';
